ff00c32d6ce4b8a54b31da550cff02d4ff2e3595
HalixenLuis <luguro.java@gmail.com>
2023-04-03 15:10:16 +0200

    First

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,30 @@
 import Card from './Card';
 
-export default function Cards(props) {
-   const { characters } = props;
+export default function Cards({ characters }) {
 
    return (
       <div>
-      {characters.map((character) => (
-         <Card
-          key={character.id}
-          name={character.name}
-          image={character.image}
-          status={character.status}
-          species={character.species}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
-      ))}
+    {
+      characters.map(({id, name, status, species, gender, origin, image,
+      }) => {
+         return(
+            <Card
+               key={id}
+               id={id}
+               name={name}
+               status={status}
+               species={species}
+               gender={gender}
+               origin={origin.name}
+               image={image}
+               onClose={() => alert('Emulamos que se cierra la card')}
+            />
+         )
+      })
+    }
       </div>
   );
-}
+};
 
 
 
